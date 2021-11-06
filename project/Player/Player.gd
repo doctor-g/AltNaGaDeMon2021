4cfdc59a9830ae75e354760718c2c51e7a636be5
Paul Gestwicki <pvgestwicki@bsu.edu>
2021-11-06 08:08:59 -0400

    Add duck

--- project/Player/Player.gd
+++ project/Player/Player.gd
@@ -7,22 +7,33 @@ var _gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var _velocity := Vector2.ZERO
 
 onready var _sprite := $AnimatedSprite
+onready var _duck_sprite := $DuckSprite
 
 func _physics_process(_delta):
 	_velocity.y += _gravity
 	
-	var direction := Input.get_action_strength("move_right") \
-					 - Input.get_action_strength("move_left")
+	if Input.is_action_pressed("duck"):
+		_sprite.visible = false
+		_duck_sprite.visible = true
+	else:
+		_sprite.visible = true
+		_duck_sprite.visible = false
+		_process_movement_input()
 	
+	_velocity = move_and_slide(_velocity, Vector2.UP)
+
+
+func _process_movement_input()->void:
+	var direction := Input.get_action_strength("move_right") \
+						 - Input.get_action_strength("move_left")
 	_velocity.x = speed * direction
 	if _velocity.x != 0:
 		_sprite.flip_h = _velocity.x < 0
+		_duck_sprite.flip_h = _velocity.x < 0
 		
 	if Input.is_action_just_pressed("jump"):
 		_velocity.y -= jump_strength
 	
 	_sprite.play("walk" if _velocity.x != 0 else "idle")
 	
-	
-	
-	_velocity = move_and_slide(_velocity, Vector2.UP)
+
