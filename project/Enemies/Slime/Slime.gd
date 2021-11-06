d09cd265cd88cf52e3452e5bdacec9e0245abb8c
Paul Gestwicki <pvgestwicki@bsu.edu>
2021-11-06 14:38:14 -0400

    Make slime turn when hitting wall

--- project/Enemies/Slime/Slime.gd
+++ project/Enemies/Slime/Slime.gd
@@ -6,6 +6,9 @@ var captured := false
 
 var _gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var _velocity := Vector2.ZERO
+var _moving_right := false
+
+onready var _sprite := $AnimatedSprite
 
 func _ready():
 	# The slime defaults to facing left
@@ -16,3 +19,7 @@ func _physics_process(_delta):
 	if not captured:
 		_velocity.y += _gravity
 		_velocity = move_and_slide(_velocity, Vector2.UP)
+		if is_on_wall():
+			_moving_right = not _moving_right
+			_velocity.x = speed if _moving_right else -speed
+		_sprite.flip_h = _moving_right
