54c9d38b109750bdf1ab2f0932b8bc0b9a111e17
Paul Gestwicki <pvgestwicki@bsu.edu>
2021-11-06 13:34:12 -0400

    Add a slime that walks off the edge of the world

---
+++ project/Enemies/Slime/Slime.gd
@@ -0,0 +1,16 @@
+extends KinematicBody2D
+
+export var speed := 100
+
+var _gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var _velocity := Vector2.ZERO
+
+func _ready():
+	# The slime defaults to facing left
+	_velocity.x = -speed
+
+
+func _physics_process(_delta):
+	_velocity.y += _gravity
+	
+	_velocity = move_and_slide(_velocity, Vector2.UP)
