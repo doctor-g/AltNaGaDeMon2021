c081e4d4a620b30abbaf25acb073aa392d686a4d
Paul Gestwicki <pvgestwicki@bsu.edu>
2021-11-06 16:54:57 -0400

    Release the enemy if the orb is not kicked in time
    This also includes a fix for the multi-orb problem.

--- project/Player/Orb/Orb.gd
+++ project/Player/Orb/Orb.gd
@@ -4,10 +4,15 @@ const _NUMBER_OF_POINTS := 30
 const _TWEEN_TIME := 0.2
 const _PLAYER_LAYER := 1
 
+const _COLOR := Color.aliceblue
+const _WARNING_COLOR := Color.yellow
+
 export var speed := 600
 
 # How many bounces before the orb disappears
 export var max_bounces := 4
+
+var color := _COLOR
 
 var _gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var _velocity := Vector2.ZERO
@@ -15,12 +20,19 @@ var _kicked := false
 var _moving_right := false
 var _bounces := 0
 
-onready var _enemy_overlap_area := $EnemyOverlapArea
+var _captured_enemy : KinematicBody2D = null
 
+# These allow us to store values that can be restored if the enemy is released
+var _enemy_parent : Node2D = null
+var _enemy_layer : int
+var _enemy_mask : int
+
+onready var _enemy_overlap_area := $EnemyOverlapArea
+onready var _anim_player := $AnimationPlayer
 
 func _draw():
 	var radius = $CollisionShape2D.shape.radius
-	draw_arc(Vector2.ZERO, radius, 0, TAU, _NUMBER_OF_POINTS, Color.aliceblue, 4)
+	draw_arc(Vector2.ZERO, radius, 0, TAU, _NUMBER_OF_POINTS, color, 4)
 
 
 func _physics_process(_delta):
@@ -37,6 +49,13 @@ func _physics_process(_delta):
 				_enemy_overlap_area.monitoring = true
 				set_collision_mask_bit(_PLAYER_LAYER, false)
 				_velocity.x = speed
+				
+				# Stop the animation player so we don't expire.
+				# Also make sure the color is right and redraw,
+				# since it can be animated.
+				_anim_player.stop(true)
+				color = _COLOR
+				update()
 				
 				# Adjust direction based on if this was hit from left or right
 				if collision.position.x > global_position.x:
@@ -61,13 +80,19 @@ func _physics_process(_delta):
 
 
 func capture(enemy:KinematicBody2D)->void:
+	assert(not enemy.captured, "Enemy already captured!")
+	_captured_enemy = enemy
+	_enemy_parent = enemy.get_parent()
+	_enemy_layer = enemy.collision_layer
+	_enemy_mask = enemy.collision_mask
+	
 	enemy.collision_mask = 0
 	enemy.collision_layer = 0
 	enemy.captured = true
 	
 	# Need to store the position because reparenting
 	var enemy_position := enemy.global_position
-	enemy.get_parent().remove_child(enemy)
+	_enemy_parent.remove_child(enemy)
 	add_child(enemy)
 	enemy.global_position = enemy_position
 
@@ -82,8 +107,33 @@ func capture(enemy:KinematicBody2D)->void:
 	tween.start()
 
 
+func toggle_color():
+	color = _WARNING_COLOR if color==_COLOR else _COLOR
+	update() # Force redraw
+
+
 # When this sensor is active and runs into an enemy,
 # that means the orb was kicked, and we should damage
 # the enemy we hit
 func _on_EnemyOverlapArea_body_entered(body):
 	body.damage()
+
+
+func _on_AnimationPlayer_animation_finished(_anim_name):
+	# Restore parent while keeping position
+	var pos = _captured_enemy.global_position
+	remove_child(_captured_enemy)
+	_enemy_parent.add_child(_captured_enemy)
+	_captured_enemy.global_position = pos
+	_captured_enemy.captured = false
+	
+	# Restore collision settings
+	_captured_enemy.collision_layer = _enemy_layer
+	_captured_enemy.collision_mask = _enemy_mask
+	
+	# Clear instance variables. Maybe not necessary, but it
+	# seems like it should help the garbage collector.
+	_captured_enemy = null
+	_enemy_parent = null
+	
+	queue_free()
--- project/Player/Projectile/Projectile.gd
+++ project/Player/Projectile/Projectile.gd
@@ -10,15 +10,20 @@ func _physics_process(_delta):
 	# warning-ignore:return_value_discarded
 	move_and_slide(direction * speed, Vector2.UP)
 	for i in range(0,get_slide_count()):
-			var collision := get_slide_collision(i)
-			if collision.collider.is_in_group("enemies"):
-				var enemy : KinematicBody2D = collision.collider
-				var orb : KinematicBody2D = _Orb.instance()
-				orb.set_as_toplevel(true)
-				orb.global_position = collision.position
-				get_parent().add_child(orb)
-				orb.capture(enemy)
-				queue_free()
+		var collision := get_slide_collision(i)
+		if collision.collider.is_in_group("enemies"):
+			var enemy : KinematicBody2D = collision.collider
+			var orb : KinematicBody2D = _Orb.instance()
+			orb.set_as_toplevel(true)
+			orb.global_position = collision.position
+			get_parent().add_child(orb)
+			orb.capture(enemy)
+			queue_free()
+			
+			# Note: If the loop is allowed to continue, it is possible
+			# to accidentally create more than one orb. This break ensures
+			# that cannot happen.
+			break
 	
 	if is_on_wall():
 		queue_free()
