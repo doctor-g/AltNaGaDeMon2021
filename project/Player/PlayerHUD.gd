972bdea612393c4af3ca33d2822f0d9dd396ac6b
Paul Gestwicki <pvgestwicki@bsu.edu>
2021-11-08 14:53:34 -0500

    Animate score gains

--- project/Player/PlayerHUD.gd
+++ project/Player/PlayerHUD.gd
@@ -1,5 +1,6 @@
 extends VBoxContainer
 
+const _TWEEN_SPEED := 0.3
 const _ICONS = [
 	preload("res://Player/Pawn/P1Images/hud_p1Alt.png"),
 	preload("res://Player/Pawn/P2Images/hud_p3Alt.png")
@@ -7,8 +8,11 @@ const _ICONS = [
 
 var player
 
+var _displayed_score := 0
+
 onready var _lives_box := $LivesBox
 onready var _score_label := $ScoreLabel
+onready var _tween : Tween = $ScoreTween
 
 func _ready():
 	assert (player != null, "Player must be set during initialization")
@@ -22,10 +26,16 @@ func _ready():
 	player.connect("score_changed", self, "_on_Player_score_changed")
 
 
+func _process(_delta):
+	_score_label.text = str(_displayed_score)
+	
+
 func _on_Player_lives_changed(_new_lives:int)->void:
 	var life_icon = _lives_box.get_child(0)
 	_lives_box.remove_child(life_icon)
 
-
 func _on_Player_score_changed(new_score:int)->void:
-	_score_label.text = str(new_score)
+	# warning-ignore:return_value_discarded
+	_tween.interpolate_property(self, "_displayed_score", _displayed_score, new_score, _TWEEN_SPEED)
+	# warning-ignore:return_value_discarded
+	_tween.start()
