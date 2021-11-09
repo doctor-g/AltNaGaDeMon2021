2d6589c950c69fe6aa1e483445ccdaf6216fed55
Paul Gestwicki <pvgestwicki@bsu.edu>
2021-11-09 07:44:58 -0500

    Replace join control with simpler navigable buttons

--- project/Screens/MainMenu.gd
+++ project/Screens/MainMenu.gd
@@ -2,14 +2,24 @@ extends Control
 
 const _LEVEL := preload("res://World/Level.tscn")
 
-func _on_JoinControl_complete():
-	var joined_players := 0
-	for control in $JoinControls.get_children():
-		if control.joined:
-			joined_players += 1
-	
+
+func _ready():
+	$ButtonBox/Start1PButton.grab_focus()
+
+
+func _on_Start1PButton_pressed():
+	_start_game(1)
+
+
+func _on_Start2PButton_pressed():
+	_start_game(2)
+
+
+func _start_game(num_players:int)->void:
+	assert (num_players>0)
 	var level := _LEVEL.instance()
-	level.num_players = joined_players
+	level.num_players = num_players
 	get_tree().get_root().add_child(level)
 	get_tree().get_root().remove_child(self)
-	
+
+
