dca471867cdb2d1af1e21cca05514cd07eb716f2
Paul Gestwicki <pvgestwicki@bsu.edu>
2021-11-24 21:26:19 -0500

    Give button the focus
    This prevents a problem where the ui was still being navigated and activated
    'under' the modal dialog

--- project/UI/OptionsMenu.gd
+++ project/UI/OptionsMenu.gd
@@ -1,4 +1,11 @@
 extends PopupDialog
 
+onready var _ok_button := $CenterContainer/VBoxContainer/OkButton
+
+func show_modal(exclusive:bool=false)->void:
+	.show_modal(exclusive)
+	_ok_button.grab_focus()
+
+
 func _on_OkButton_pressed():
 	visible = false
