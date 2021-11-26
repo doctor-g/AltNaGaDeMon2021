0398bb1648a5b5b7d641b7e913a701f131ff5a43
Paul Gestwicki <pvgestwicki@bsu.edu>
2021-11-26 08:22:51 -0500

    Add fullscreen toggle
    Fixes #19

--- project/UI/GameOptions.gd
+++ project/UI/GameOptions.gd
@@ -4,10 +4,12 @@ onready var _music_bus := AudioServer.get_bus_index("Music")
 onready var _music_slider := $MusicVolume
 onready var _sfx_bus := AudioServer.get_bus_index("SFX")
 onready var _sfx_slider := $SFXVolume
+onready var _fullscreen_checkbox := $FullscreenCheckbox
 
 func _ready():
 	_music_slider.value = db2linear(AudioServer.get_bus_volume_db(_music_bus))
 	_sfx_slider.value = db2linear(AudioServer.get_bus_volume_db(_sfx_bus))
+	_fullscreen_checkbox.pressed = OS.window_fullscreen
 
 
 func _on_MusicVolume_value_changed(value:float)->void:
@@ -16,3 +18,7 @@ func _on_MusicVolume_value_changed(value:float)->void:
 
 func _on_SFXVolume_value_changed(value:float)->void:
 	AudioServer.set_bus_volume_db(_sfx_bus, linear2db(value))
+
+
+func _on_FullscreenCheckbox_toggled(button_pressed):
+	OS.window_fullscreen = button_pressed
